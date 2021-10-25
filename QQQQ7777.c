7bf91845625d86bcefda63dfc9b1d09580baf775
VSCUser999 <def8663@gmail.com>
2021-10-26 01:10:27 +0800

    HM20211026_0110

---
+++ QQQQ7777.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+unsigned long long int  func1(int x){
+    unsigned long long int r = 1;
+    int i,k;
+    for(i = 1; i <= x; i++){
+        r *= i;
+    }
+    return r;
+}
+
+void func2(unsigned long long p){
+    int k = 0;
+    while( (p % 10) == 0){
+        p = p / 10;
+        k++;
+    }
+    printf("%d\n",k);
+}
+
+int main (void){
+    unsigned long long y;
+    int x;
+    scanf("%d",&x);
+    y = func1(x);
+    func2(y);
+}
